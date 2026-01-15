57209a477a1e2aab525902d181736f5b8d044eec
lukdavid <lucas.david@a-grid.com>
2026-01-15 16:22:33 +0100

    Feat(api): POST /drivers - create driver

--- packages/api/src/api/routes/drivers.py
+++ packages/api/src/api/routes/drivers.py
@@ -1,10 +1,11 @@
 from typing import Annotated
 
 from core.devices_manager import DevicesManager
-from dto.driver_dto import DriverDTO, core_to_dto
+from dto.driver_dto import DriverDTO, core_to_dto, dto_to_core
 from fastapi import APIRouter, Depends, HTTPException
+from storage import CoreFileStorage
 
-from api.dependencies import get_device_manager
+from api.dependencies import get_device_manager, get_repository
 
 router = APIRouter()
 
@@ -27,3 +28,19 @@ def get_driver(
         raise HTTPException(
             status_code=404, detail=f"Driver {driver_id} not found"
         ) from ke
+
+
+@router.post("/", status_code=201)
+def create_driver(
+    driver_dto: DriverDTO,
+    dm: Annotated[DevicesManager, Depends(get_device_manager)],
+    repository: Annotated[CoreFileStorage, Depends(get_repository)],
+) -> DriverDTO:
+    if driver_dto.id in dm.drivers:
+        raise HTTPException(
+            status_code=409, detail=f"Driver {driver_dto.id} already exists"
+        )
+    driver = dto_to_core(driver_dto)
+    dm.drivers[driver_dto.id] = driver
+    repository.drivers.write(driver_dto.id, driver_dto)
+    return driver_dto
