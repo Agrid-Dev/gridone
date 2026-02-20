5baa71c06b1e4682f2898cc0ff040e6bbfae1e84
lukdavid <lucas.david@a-grid.com>
2026-02-20 18:18:13 +0100

    design(UI): device chart polish

--- apps/ui/src/pages/devices/device/device-history/DeviceHistoryChart.tsx
+++ apps/ui/src/pages/devices/device/device-history/DeviceHistoryChart.tsx
@@ -96,7 +96,6 @@ export default function DeviceHistoryChart() {
       booleanValues={booleanValues}
       stringSeries={stringSeries}
       stringValues={stringValues}
-      height={400}
     />
   );
 }
