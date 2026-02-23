7e533ce16732d06b56a7dd1d71e78cc2c08c4ec2
agrid <lucas.david@a-grid.com>
2026-02-23 16:51:11 +0000

    feat: add value_adapter to support LoRa devices

--- packages/devices_manager/src/devices_manager/core/value_adapters/factory.py
+++ packages/devices_manager/src/devices_manager/core/value_adapters/factory.py
@@ -8,6 +8,7 @@ from .registry.byte_convert_adapter import byte_convert_adapter
 from .registry.identity_adapter import identity_adapter
 from .registry.json_path_adapter import json_path_adapter
 from .registry.json_pointer_adapter import json_pointer_adapter
+from .registry.lorawan_elsys_adapter import lorawan_elsys_adapter
 from .registry.scale_adapter import scale_adapter
 
 value_adapter_builders = {
@@ -17,6 +18,7 @@ value_adapter_builders = {
     "json_path": json_path_adapter,
     "bool_format": bool_format_adapter,
     "byte_convert": byte_convert_adapter,
+    "lorawan_elsys": lorawan_elsys_adapter,
 }
 
 supported_value_adapters = list(value_adapter_builders.keys())
