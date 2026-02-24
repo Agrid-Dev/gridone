97a49fcfeb275e8f9d52619ca0b05eeab73dd53d
lukdavid <lucas.david@a-grid.com>
2026-02-24 17:38:47 +0100

    feat(api): plug storage url to enable postgres storage in timeseries

--- packages/timeseries/src/timeseries/__init__.py
+++ packages/timeseries/src/timeseries/__init__.py
@@ -1,12 +1,19 @@
+import logging
+
 from models.errors import InvalidError, NotFoundError
 
 from timeseries.domain import DataPoint, DataType, SeriesKey
 from timeseries.service import TimeSeriesService
 from timeseries.storage import build_storage
 
+logger = logging.getLogger(__name__)
+
 
 async def create_service(db_url: str | None = None) -> TimeSeriesService:
     storage = await build_storage(db_url)
+    logger.info(
+        "Starting time series service with storage %s", storage.__class__.__name__
+    )
     return TimeSeriesService(storage)
 
 
