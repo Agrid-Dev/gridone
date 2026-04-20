ae759bf1fde54bc60148f047ac0c03d030a84890
Mihir <mihir.sanghvi@polytechnique.edu>
2026-04-20 15:48:01 +0200

    fix(dm): guard migration backfill against missing device_asset_links table

--- packages/devices_manager/src/devices_manager/storage/postgres/migrations/0003.devices-manager-tags.sql
+++ packages/devices_manager/src/devices_manager/storage/postgres/migrations/0003.devices-manager-tags.sql
@@ -3,7 +3,15 @@
 ALTER TABLE dm_devices
     ADD COLUMN tags JSONB NOT NULL DEFAULT '{}';
 
-UPDATE dm_devices d
-SET tags = jsonb_build_object('asset_id', jsonb_build_array(dal.asset_id))
-FROM device_asset_links dal
-WHERE dal.device_id = d.id;
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1 FROM information_schema.tables
+        WHERE table_name = 'device_asset_links'
+    ) THEN
+        UPDATE dm_devices d
+        SET tags = jsonb_build_object('asset_id', jsonb_build_array(dal.asset_id))
+        FROM device_asset_links dal
+        WHERE dal.device_id = d.id;
+    END IF;
+END $$;
